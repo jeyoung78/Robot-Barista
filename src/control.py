8d3722febaa4e35a82faea2505e97f6129e34ff6
mud-the-developer <mudmud611@yonsei.ac.kr>
2025-02-24 14:04:11 +0900

    communication error solve

--- src/control.py
+++ src/control.py
@@ -1,30 +1,24 @@
-# Robot commands go here
-import socket 
+import socket
 
 class Communicate:
-    def __init__(self, robot_ip = '192.168.137.100', robot_port = 20002):
+    def __init__(self, robot_ip='192.168.137.100', robot_port=20002):
         self.robot_ip = robot_ip
         self.robot_port = robot_port
-        self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def communicate(self, command):
-        self.client_sock.connect((self.robot_ip, self.robot_port))
-        self.client_sock.sendall(command.encode('utf-8'))
-        
+        # Create a new socket for each command
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_sock:
+            client_sock.connect((self.robot_ip, self.robot_port))
+            client_sock.sendall(command.encode('utf-8'))
+
     def move_x(self, pos_dir=True):
-        if pos_dir == True:
-            self.communicate("move_x_positive")
-        else:
-            self.communicate("move_x_negative")
+        command = "move_x_positive" if pos_dir else "move_x_negative"
+        self.communicate(command)
 
     def move_y(self, pos_dir=True):
-        if pos_dir == True:
-            self.communicate("move_y_positive")
-        else:
-            self.communicate("move_y_negative")
+        command = "move_y_positive" if pos_dir else "move_y_negative"
+        self.communicate(command)
 
     def move_z(self, pos_dir=True):
-        if pos_dir == True:
-            self.communicate("move_z_positive")
-        else:
-            self.communicate("move_z_negative")
+        command = "move_z_positive" if pos_dir else "move_z_negative"
+        self.communicate(command)
